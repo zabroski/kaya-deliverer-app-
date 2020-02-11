43e1fdccf00f2ef9126f7a5c97d08ead1337855e
Issouf Zabre <sheridanzabre@gmail.com>
2020-02-11 17:46:15 -0500

    conform pick and droppOff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { getDeliveries, confirmPickup,confirmDropOff, getDeliverer} from './lib/apiService';
+import { getDeliveries, getDeliverer} from './lib/apiService';
+import Delivery from './components/Delivery';
 
 const handleGetDeliveries = async (setDeliveries) => {
   const deliveries = await getDeliveries();
@@ -8,67 +9,23 @@ const handleGetDeliveries = async (setDeliveries) => {
   setDeliveries(deliveries);
 }
 
-
-// const handleConfirmPickUp = async (setPickUp) => {
-//   const pickUp = await confirmPickup();
-//   setPickUp(pickUp)
-// }
-
-
-
 function App() {
 
-  // this.state = {
-  //   deliveries: [],
-  //      user: {}
-  // };
-
   let [deliveries, setDeliveries] = useState([]);
-  // let [pickUp, setPickUp ] = useState([])
 
   useEffect(() => {
     handleGetDeliveries(setDeliveries);
   }, []);
-
-  // useEffect(() => {
-  //   handleConfirmPickUp(setPickUp);
-  // }, []);
 
   return (
     <div className="App">
       <h1>Deliveries</h1>
       {deliveries.map(delivery => {
       return (
+        <div>
+          <Delivery delivery={delivery} />
+        </div>
 
-        <>
-          <h3>{delivery.id}, {delivery.status}</h3>
-
-          {delivery.merchant&& 
-            <>
-              {delivery.merchant.lastName},
-              {delivery.merchant.firstName}
-            </>}
-
-            {delivery.addresses.map((address) => {
-              return (
-                <ul>
-                  <li>{address.type}</li>
-                  <li>{address.street}</li>
-                  <li>{address.zipCode}</li>
-                  <li>{address.country}</li>
-                  <li>{address.city}</li>
-                </ul>
-              )
-
-            })}
-            <button onClick={async () => {
-              await confirmPickup(delivery.id);
-            }}>Accept</button>
-
-            <button onClick={async () => {
-              await confirmDropOff(delivery.id);
-            }}>Complet delivery</button>
-        </>
       );
       
       })}
---
+++ src/components/Delivery/index.js
@@ -0,0 +1,51 @@
+import React,{useState, useEffect} from 'react';
+import {confirmPickup,confirmDropOff} from '../../lib/apiService';
+
+function Delivery(props) {
+    
+    let delivery = props.delivery;
+    let [status, setStatus] = useState([]);
+
+    useEffect(() => {
+        setStatus(delivery.status);
+    }, [delivery]); // line 6
+
+    return (
+        <div>
+             <h3>{delivery.id}, {delivery.status}</h3>
+
+            {delivery.merchant && 
+            <>
+                {delivery.merchant.lastName},
+                {delivery.merchant.firstName}
+            </>}
+
+
+
+            {delivery.addresses.map((address) => {
+                return (
+                <ul>
+                    <li>{address.type}</li>
+                    <li>{address.street}</li>
+                    <li>{address.zipCode}</li>
+                    <li>{address.country}</li>
+                    <li>{address.city}</li>
+                </ul>
+                )
+
+            })}
+            {status === "" && <button onClick={async () => {
+                await confirmPickup(delivery.id);
+                setStatus("in transit");
+            }}>Accept</button>}
+
+            {status === "in transit" && 
+            <button onClick={async () => {
+                await confirmDropOff(delivery.id);
+                setStatus("done");
+            }}>Complet delivery</button>}
+        </div>
+ )
+}
+
+export default Delivery;
