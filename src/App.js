2fa1c6f1ecb072d047ec8b652fcb264e53d94af0
Issouf Zabre <sheridanzabre@gmail.com>
2020-12-16 00:49:13 -0500

    connect api with front end

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import './App.css';
 import HistoryPage from './components/HistoryPage';
 import TransitDeliveries from './components/TrasitDeliveries'
 import DashboardPage from './components/DashboardPage';
-import SignupPage from './components/SignupPage';
-import LoginPage from  './components/LoginPage';
 import { Route, Link} from "react-router-dom";
-import  ProtectedRoute from './components/ProtectedRoute';
-//  import { login, getProfile, signUp } from './lib/apiService';
- import authService from './lib/authService';
-
 
  class App extends React.Component {
   constructor(props) {
@@ -24,29 +18,17 @@ import  ProtectedRoute from './components/ProtectedRoute';
   }
   
 
-signOutDeliverere = () => {
-  authService.signOut()
+// openNav = () => {
+//   document.getElementById("mySidenav").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+//   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+// }
 
-  this.setState(state => {
-    return {
-      isSignedIn: false,
-      deliverer: {}
-    }
-  })
-}
-
-
-openNav = () => {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-closeNav = () => {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
-}
+// closeNav = () => {
+//   document.getElementById("mySidenav").style.width = "0";
+//   document.getElementById("main").style.marginLeft = "0";
+//   document.body.style.backgroundColor = "white";
+// }
 
 
 
@@ -56,25 +38,13 @@ render() {
 
   return (
     <div className="App">
-        {authService.isAuthenticated()}
+        
         <nav className="nav-menu">
-          {/* {authService.isAuthenticated() && <Link to='/'>Dashboard</Link>}
-          {authService.isAuthenticated() && <Link to="/deliveries-history">HISTORY</Link>}
-          {authService.isAuthenticated() && <Link to="/transit-deliveries">ANONYM</Link>}
-          {!authService.isAuthenticated() ?  <Link to="/login">SIGN IN</Link> :
-          <button className="signUp-button" onClick= {this.signOutDeliverere}>Sign Out</button>
-          }  */}
-
-          {/* {/* {
-          !authService.isAuthenticated() ? (
-            <Link to="/signup">REGISTER</Link>
-          ) : (
-            null
-          )
-          }
-            
-{/*  */}
-
+          { <Link to='/'>Dashboard</Link>}
+          {<Link to="/deliveries-history">HISTORY</Link>}
+          { <Link to="/transit-deliveries">ANONYM</Link>}
+       
+{/*      
           <div id="mySidenav" class={`sidenav ${!this.state.isMenuOpen? "sidenavClose": ""}`}>
           <button href="#" class="closebtn" onClick={(e) => {
             e.preventDefault();
@@ -84,51 +54,37 @@ render() {
               isMenuOpen: !this.state.isMenuOpen
             });
           }}>&times;</button>
-          {authService.isAuthenticated() && <Link to='/'>Dashboard</Link>}
-          {authService.isAuthenticated() && <Link to="/deliveries-history">HISTORY</Link>}
-          {authService.isAuthenticated() && <Link to="/transit-deliveries">TRANSIT</Link>}
-          {!authService.isAuthenticated() ?  <Link to="/login">SIGN IN</Link> :
-          <button className="signUp-button" onClick= {this.signOutDeliverere}>Sign Out</button>}
-
-          {!authService.isAuthenticated() ? (<Link to="/signup">REGISTER</Link>) : (null)}
-          </div>
-          <button className="open-button" onClick={(e) => {
+          { <Link to='/'>Dashboard</Link>}
+          {<Link to="/deliveries-history">HISTORY</Link>}
+          {<Link to="/transit-deliveries">TRANSIT</Link>}
+          </div> */}
+          {/* <button className="open-button" onClick={(e) => {
             e.preventDefault();
             
             this.setState({
               ...this.state,
               isMenuOpen: !this.state.isMenuOpen
             });
-          }}>open</button>
+          }}>open</button> */}
           <div id="main">...</div> 
         </nav>
         <main className="main-menu">
 
-          <Route 
-            exact={true}
-            path="/login"
-            render={(props) => <LoginPage  {...props}/* handleLogin={this.loginDeliverer} isSignedIn={isSignedIn}*//>} 
-          />
-          <Route 
-            exact={true}
-            path='/signup' 
-            render = {(props) => <SignupPage {...props} /*handleSignup={this.signUpDeliverer}*/ /*isSignedIn={isSignedIn}*/ />}
-          />
-          <ProtectedRoute 
+          <Route
             exact={true}
             path='/'
             deliverer={deliverer} 
             component={DashboardPage}
           />
 
-          <ProtectedRoute 
+          <Route
             exact={true}
             path='/deliveries-history'
             deliverer={deliverer} 
             component={HistoryPage}
           />
 
-          <ProtectedRoute 
+          <Route 
             exact={true}
             path='/transit-deliveries'
             deliverer={deliverer} 
--- src/lib/apiService.js
+++ src/lib/apiService.js
@@ -2,47 +2,10 @@ import axios from 'axios'
 
 const BASE_URL = process.env.API_URL || "http://localhost:3000";
 
-const JWT_TOKEN = localStorage.getItem('token')
 const axiosClient = axios.create({
     baseURL: BASE_URL,
-    headers: {
-        'Authorization': `Bearer ${JWT_TOKEN}`
-
-    }
+  
 })
-
-
-export const signUp = async (data) => {
-
-    try {
-        console.log('RIGHT HERERRE',data)
-        const response = await axiosClient.post('/auth/signup', data)
-       
-        const { token, deliverer } = response.data
-       
-        localStorage.setItem('token', token)
-        return deliverer
-      
-
-    } catch(e) {
-        throw e
-    }
-};
-
-
-export const login = async (data) => {
-    try {
-        const response = await axiosClient.post('/auth/login', data)
-        const { token, deliverer } = response.data
-
-        await localStorage.setItem('token', token)
-        return deliverer
-
-    } catch(e) {
-        throw e
-    }
-}
-
 
 
 export const getDeliverer = async (delevererId) => {
